70cf8c96a4167fcbce6a5b817c44c983adf37649
murilomontino <murilomontinojr@hotmail.com>
2022-05-01 14:49:23 -0300

    [chore] new dependencies

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,20 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+      "module-resolver", 
+      {
+        alias: {
+          '@/screens': "./source/screens",
+          '@/components': "./source/components",
+          '@/assets': "./source/assets",
+          '@/theme': "./source/constants",
+          '@/reducers': "./source/reducers",
+          '@/services': "./source/services",
+          '@/navigation': "./source/navigation",
+        }
+      }]
+    ]
   };
 };
